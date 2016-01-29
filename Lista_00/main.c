f4a1e1c3b0a07c35ed9decf26f9a76a8aaebab77
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-01-29 13:37:51 -0300

    Segunda OK

--- Lista_00/main.c
+++ Lista_00/main.c
@@ -72,7 +72,7 @@ void mostraVetor(int *vetor, int dimensao){
 }
 
 int mainQuestao2(){
-	int dimensaoX, dimensaoY, dimensaoW, i, menorX, maiorX;
+	int dimensaoX, dimensaoY, dimensaoW, i, menorX, maiorX, z, achou;
 	int vetorX[N], vetorY[N], vetorW[N];
 	printf("Digite a dimensão do vetor X, no máximo %d:", N);
 	scanf("%d", &dimensaoX);
@@ -83,7 +83,14 @@ int mainQuestao2(){
 
 	dimensaoY = 0;
 	dimensaoW = 0;
+	menorX = vetorX[0];
+	maiorX = vetorX[0];
 	for(i = 0; i < dimensaoX; i++){
+
+		if(vetorX[i] > maiorX)
+			maiorX = vetorX[i];
+		if(vetorX[i] < menorX)
+			menorX = vetorX[i];
 		if(vetorX[i] > 10 && vetorX[i] < 40){
 			vetorY[dimensaoY] = vetorX[i];
 			dimensaoY++;
@@ -95,9 +102,28 @@ int mainQuestao2(){
 		}
 	}
 
-
 	printf("Vetor X:\n");
+	mostraVetor(vetorX, dimensaoX);
+	printf("Vetor Y:\n");
+	mostraVetor(vetorY, dimensaoY);
+	printf("Vetor W:\n");
+	mostraVetor(vetorW, dimensaoW);
 
+	printf("Maior elemento de X: %d\n", maiorX);
+	printf("Menor elemento de X: %d\n", menorX);
+
+	do{
+		printf("Digite o numero Z que deseja pesquisar: ");
+		scanf("%d", &z);
+		achou = 0;
+		for(i = 0; i < dimensaoX; i++){
+			if(vetorX[i]== z){
+				achou = 1;
+				printf("Elemento %d encontrado. ", vetorX[i]);
+			}
+		}
+	}while(!achou);
+	printf("Fim da aplicação.");
 
 
 	return 0;

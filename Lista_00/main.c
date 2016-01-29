0dd3d05a2a89addbea0e33846b746d159236937e
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-01-29 11:25:13 -0300

    Ajeitando switch case

--- Lista_00/main.c
+++ Lista_00/main.c
@@ -1,49 +1,55 @@
 #include <stdio.h>
 
-
-int mainQuestao1(){
-
-
-	int lista[100];
-	int dimensao, i;
-
-
-	printf("Digite a dimensão, no máximo 100:");
-	scanf("%d", dimensao);
-	for(i = 0; i < dimensao; i++){
-		printf("Digite o elemento %d\n", i);
-		scanf("%d", lista[i]);
-		if(lista[i] >= 256)
-			printf("Digite valores menores que 256\n");
-	}
-
-
-
-
-
-
-	return 0;
+int mainQuestao1() {
+//
+//	int lista[100];
+//	int dimensao, i;
+//
+//	printf("Digite a dimensão, no máximo 100:");
+//	scanf("%d", dimensao);
+//	for (i = 0; i < dimensao; i++) {
+//		printf("Digite o elemento %d\n", i);
+//		scanf("%d", lista[i]);
+//		if (lista[i] >= 256)
+//			printf("Digite valores menores que 256\n");
+//	}
+//	return 0;
 }
 
-int main(){
+int main() {
 	int questao;
 
-
-	do{
-		printf("Digite um número entre 0 e 5 para escolher uma questão ou 6 para sair.");
+	do {
+		printf("Digite um número entre 0 e 5 ou 0 para sair.\n");
 		scanf("%d", &questao);
 
-		switch(questao){
-			case 1:
-				mainQuestao1();
-				break;
-			default:
-				print("Questão não encontrada.");
-				break;
+		switch (questao) {
+		case 0:
+			break;
+		case 1:
+			mainQuestao1();
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+
+		default:
+			printf("Comando não encontrado\n");
+			break;
+
 		}
 
-	}while(questao != 6);
+	} while (questao);
 
-	printf("Teste");
+	printf("Fim do programa!!!\n\n\n");
 	return 0;
 }

7992134fd9d4d419ad101c9da7bb96c1c8fe860d
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-01-30 00:20:08 -0300

    Terceira OK

--- Lista_00/main.c
+++ Lista_00/main.c
@@ -121,7 +121,7 @@ int mainQ2(){
 int mainQ3(){
     int dimensaoA, dimensaoC, i;
     int *vetorA, *vetorB, *vetorC;
-    printf("Digite a dimensão do Vetor A:");
+    printf("Digite a dimensão para vetores A e B:");
     scanf("%d", &dimensaoA);
     dimensaoC = 2*dimensaoA;
     vetorA = malloc(sizeof(int)*dimensaoA);
@@ -132,19 +132,30 @@ int mainQ3(){
         printf("Memoria Insuficiente");
         return 0;
     }
+    printf("Recebendo valores do Vetor A:\n");
     for(i = 0; i < dimensaoA; i++){
+        printf("Digite o elemento %d:\n", i);
         scanf("%d", vetorA+i);
     }
+    printf("Recebendo valores do Vetor B:\n");
     for(i = 0; i < dimensaoA; i++){
+        printf("Digite o elemento %d:\n", i);
         scanf("%d", vetorB+i);
-
     }
 
 
     printf("Vetor A\n");
     mostraVetor(vetorA, dimensaoA);
     printf("Vetor B\n");
-    mostraVetor(vetorA, dimensaoA);
+    mostraVetor(vetorB, dimensaoA);
+    dimensaoC = 0;
+    for(i = 0; i < dimensaoA; i++){
+        *(vetorC+dimensaoC) = *(vetorA+i);
+        *(vetorC+dimensaoC+1) = *(vetorB+i);
+        dimensaoC += 2;
+    }
+    printf("Vetor C\n");
+    mostraVetor(vetorC, dimensaoC);
 
     return 0;
 }
@@ -166,7 +177,7 @@ int main() {
             mainQ2();
 			break;
 		case 3:
-            mainQuestao3();
+            mainQ3();
 			break;
 		case 4:
 

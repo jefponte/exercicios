cca1e431326bd8221bed5f668c5b4e4ea16a7bae
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-01-30 11:18:47 -0300

    Quinta já ta pegando vetor.

--- Lista_00/Q5/main.c
+++ Lista_00/Q5/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int dimensao, i;
+    int *lista;
+    printf("Digite o valor de N, dimensao do vetor:\n");
+    scanf("%d", &dimensao);
+    lista = malloc(sizeof(int)*dimensao);
+    if(!lista){
+        printf("Memoria insuficiente\n");
+        return 0;
+    }
+    for(i = 0; i < dimensao; i++){
+        printf("Digite o elemento %d:", i);
+        scanf("%d", (lista+i));
+    }
+
+
+
+
+
+
+
+
 }

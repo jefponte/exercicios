ab8e3fc93462fa9ed79f2b66b3c263ef5c49040c
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-01-30 21:21:00 -0300

    Já verificando repetições e retirando da lista.

--- Lista_00/Q5/main.c
+++ Lista_00/Q5/main.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define FALSE 0
+#define TRUE !FALSE
 
+void mostraVetor(int *vetor, int dimensao){
+	int i;
+	for(i = 0; i < dimensao; i++)
+		printf("Elemento [%d]: %d\n", i, vetor[i]);
+}
+
+
+void ordenacaoBolha(int *lista, int dimensao){
+    int houveTroca, i, aux;
+	do{
+		houveTroca = FALSE;
+		for(i = 0; i < (dimensao - 1); i++){
+			if(lista[i] < lista[i+1]){
+				aux = lista[i];
+				lista[i] = lista[i+1];
+				lista[i+1] = aux;
+				houveTroca = TRUE;
+			}
+		}
+	}while(houveTroca);
+}
+
+void retiraDaLista(int *lista, int *dimensao, int posicao){
+    int i;
+    printf("Quero eliminar %d\n", *(lista+posicao));
+
+
+    for(i = posicao; i < (*dimensao)-1; i++){
+        *(lista+i) = *(lista+i+1);
+    }
+    (*dimensao)--;
+}
 
 int main()
 {
-    int dimensao, i,j;
+    int dimensao, i,j, flagRepetiu, removidos;
     int *lista;
     printf("Digite o valor de N, dimensao do vetor:\n");
     scanf("%d", &dimensao);
@@ -17,18 +51,23 @@ int main()
         printf("Digite o elemento %d:", i);
         scanf("%d", (lista+i));
     }
-    //Verificar se existem repeticoes
-    for(i = 0; i < dimensao; i++){
-        for(j = 0; j < dimensao; j++){
 
+    printf("Lista como foi digitado:\n");
+    mostraVetor(lista, dimensao);
+
+    removidos = 0;
+
+    for(i = 0; i < dimensao; i++){
+        for(j = (i+1); j < dimensao; j++){
+            if(*(lista+i)== *(lista+j)){
+                removidos++;
+                retiraDaLista(lista, &dimensao, i);
+                i--;
+                break;
+            }
         }
     }
-
-
-
-
-
-
-
-
+    printf("%d removidos:\n", removidos);
+    printf("Lista depois de remover:\n");
+    mostraVetor(lista, dimensao);
 }

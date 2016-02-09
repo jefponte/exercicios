243db6b59e24d1b921c86c0c93e4c3a2de8e3bc0
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-02-09 17:39:08 -0300

    Terceira questão falta só um printf();

--- Lista_01/Q_03/main.c
+++ Lista_01/Q_03/main.c
@@ -18,12 +18,67 @@ typedef struct feriado{
     int veiculo;
 }Feriado;
 
+void lerLista(Feriado *ptr, int dimensao){
+    int i;
+    for(i = 0; i < dimensao; i++){
+        printf("Respostas motorista %d\n", i);
+        lerFeriado(ptr+i);
+    }
+
+}
+
+void lerFeriado(Feriado *ptr){
+    printf("Qual o seu destino? \n");
+    printf("1-  Camocim,\n");
+    printf("2-  Morro Branco,\n");
+    printf("3-  Aracati,\n");
+    printf("4-Guaramiranga e 5- Outros Locais;\n");
+    scanf("%d", &ptr->destino);
+    printf("Qual o veículo? \n");
+    printf("1-  Automóvel, 2-  Caminhão, 3-  Ônibus e 4-Outros Veículos");
+    scanf("%d", &ptr->veiculo);
+
+
+}
+
+void relatorio(Feriado *ptr, int dimensao){
+    int i, totalDeCaminhoes, caminhouesPraCamocim, totalDeOnibus;
+    int destinoAracati = 0;
+    totalDeCaminhoes = 0;
+    totalDeOnibus = 0;
+
+    for(i = 0; i < dimensao; i++){
+        if((ptr+i)->destino == ARACATI)
+            destinoAracati++;
+        if((ptr+i)->veiculo == CAMINHAO){
+            totalDeCaminhoes++;
+            if((ptr+i)->destino == CAMOCIM)
+                caminhouesPraCamocim++;
+
+
+        }
+        if((ptr+i)->veiculo == ONIBUS)
+            totalDeOnibus++;
+    }
+
+
+}
 
 int main()
 {
+    int dimensao;
+	Feriado *listaDeRespostas;
+	printf("Digite a dimensao:\n");
+	scanf("%d", &dimensao);
 
+	listaDeRespostas = (Feriado*)malloc(sizeof(Feriado)*dimensao);
+	if(listaDeRespostas == NULL){
+		printf("Momória insuficiente.");
+		return 0;
+	}
+    lerLista(listaDeRespostas, dimensao);
+    relatorio(listaDeRespostas, dimensao);
 
-    printf("Preguiça de continuar agora....");
 
     return 0;
 }

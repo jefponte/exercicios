47672f5596e77b3ab0d1aabc5d4b0ffd35af1928
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-02-09 17:07:02 -0300

    Fazendo a terceira, to com um pouco de preguiça, vou parar um pouco.

---
+++ Lista_01/Q_03/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CAMOCIM 1
+#define MORRO_BRANCO 2
+#define ARACATI 3
+#define GUARAMIRANGA 4
+#define OUTROS_LOCAIS 5
+
+#define AUTOMOVEL 1
+#define CAMINHAO 2
+#define ONIBUS 3
+#define OUTRO_VEICULO 4
+
+
+typedef struct feriado{
+    int destino;
+    int veiculo;
+}Feriado;
+
+
+int main()
+{
+
+
+    printf("Preguiça de continuar agora....");
+
+    return 0;
+}

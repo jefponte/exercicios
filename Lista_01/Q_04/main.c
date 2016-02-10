c41cec53be433c7269fb70c56a53865f661d5f63
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-02-10 07:25:21 -0300

    Quarta feita.

--- Lista_01/Q_04/main.c
+++ Lista_01/Q_04/main.c
@@ -39,6 +39,52 @@ void mostrarCaixa(Caixa *ptr){
 
 }
 
+void lerVetor(Caixa *ptr, int dimensao){
+    int i;
+    for(i = 0; i < dimensao; i++){
+        printf("Ler caixa %d:\n", i);
+        lerCaixa(ptr+i);
+    }
+}
+void relatorio(Caixa *ptr, int dimensao){
+    int i;
+    int despesasJaneiro = 0;
+    float saldo = 0;
+    float valor = 0;
+    float valorReceita = 0;
+    float valorDespesa = 0;
+    float valorAno = 0;
+    float despesas = 0;
+    float receitas = 0;
+
+
+    for(i = 0; i < dimensao; i++){
+        if((ptr+i)->data.mes <= 3 && (ptr+i)->data.ano <= 2005){
+            if((ptr+i)->tipo == 'D')
+                despesas += (ptr+i)->valor;
+            if((ptr+i)->tipo == 'R')
+                receitas += (ptr+i)->valor;
+
+        }
+
+        if((ptr+i)->data.ano == 2005){
+            if((ptr+i)->tipo == 'R')
+                valorReceita += (ptr+i)->valor;
+            if((ptr+i)->tipo == 'D'){
+                valorDespesa += (ptr+i)->valor;
+                if((ptr+i)->data.mes == 1)
+                    despesasJaneiro++;
+
+            }
+        }
+    }
+    valor = receitas - despesas;
+    printf("A  quantidade de lançamentos do tipo despesa realizados no mês de janeiro de 2005: %d\n",despesasJaneiro);
+    printf("A soma dos lançamentos de despesa %.2f, de receita %.2f realizados no ano de 2005\n", valorDespesa, valorReceita);
+    printf("Saldo de janeiro de 2005: %.2f\n", valor);
+
+
+}
 
 int main()
 {
@@ -53,8 +99,9 @@ int main()
         printf("Memoria nao alocada\n");
         return 0;
     }
-    lerCaixa(historicoDeCaixa);
-    mostrarCaixa(historicoDeCaixa);
+    lerVetor(historicoDeCaixa, dimensao);
+    relatorio(historicoDeCaixa, dimensao);
+
 
 
     return 0;

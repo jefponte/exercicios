dcbf06886eb44ab3d0e5951b17898eb5163c8349
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-02-20 01:32:51 -0300

    Ok

--- Lista_03/Q_01/main.c
+++ Lista_03/Q_01/main.c
@@ -18,9 +18,9 @@ void showVector(int *ptr, int dimensao){
         printf("[%d]:%d \n", i, *(ptr+i));
     }
 }
-void quickSort(int n, int *v){
 
-}
+
+
 
 void bubbleSort(int *lista, int dimensao){
     int i, j, aux;
@@ -44,13 +44,13 @@ void selectionSort(int *lista, int dimensao){
         for (j = (i+1); j < dimensao; j++) {
           if(lista[j] < lista[menor])
             menor = j;
+        }
+        if (i != menor) {
+            aux = lista[i];
+            lista[i] = lista[menor];
+            lista[menor] = aux;
+        }
     }
-    if (i != menor) {
-        aux = lista[i];
-        lista[i] = lista[menor];
-        lista[menor] = aux;
-    }
-  }
 
 }
 void insertionSort(int *lista, int tamanho)
@@ -73,42 +73,93 @@ void insertionSort(int *lista, int tamanho)
 
 
 
+void quickSort( int *vetorDesordenado, int posicaoInicio, int posicaoFim )
+{
+   int pivot;
+   if (posicaoInicio < posicaoFim)
+   {
+      pivot = partition( vetorDesordenado, posicaoInicio, posicaoFim);
+      quickSort( vetorDesordenado, posicaoInicio, pivot);
+      quickSort(vetorDesordenado, pivot+1, posicaoFim);
+   }
+}
+
+int partition( int *vetorDesordenado, int posicaoInicio, int posicaoFim )
+{
+   int x,i,j,auxiliar;
+   x = vetorDesordenado[posicaoInicio];
+   i = posicaoInicio - 1;
+   j = posicaoFim + 1;
+
+   do{
+      do {
+            j--;
+      } while( vetorDesordenado[j] > x );
+      do {
+            i++;
+      } while( vetorDesordenado[i] < x );
+
+      if (i < j)
+      {
+         auxiliar = vetorDesordenado[i];
+         vetorDesordenado[i] = vetorDesordenado[j];
+         vetorDesordenado[j] = auxiliar;
+      }
+      else
+      {
+         return j;
+      }
+   }while(1);
+}
+void loadVector(int *lista, int dimensao){
+    int i;
+    for(i = 0; i < dimensao; i++){
+        lista[i] = i-(2*i)+300;
+
+    }
+}
+
 int main()
 {
+
     int lista[DIMENSAO];
     int comando;
     printf("Alimente o vetor:\n\n");
-    readVector(lista, DIMENSAO);
+    //readVector(lista, DIMENSAO);
+    loadVector(lista, DIMENSAO);
     showVector(lista, DIMENSAO);
 
+    do{
 
+        printf("Selecione o tipo de ordenacao desejada: \n");
+        printf("1 - Bubble Sort;");
+        printf("2 - Insertion Sort;");
+        printf("3 - Selection Sort;");
+        printf("4 - QuickSort Sort");
+        scanf("%d", &comando);
 
-    printf("Selecione o tipo de ordenacao desejada: \n");
-    printf("1 - Bubble Sort;");
-    printf("2 - Insertion Sort;");
-    printf("3 - Selection Sort;");
-    printf("4 - QuickSort Sort");
-    scanf("%d", &comando);
+        switch(comando){
+            case 1:
+                bubbleSort(lista, DIMENSAO);
+                break;
+            case 2:
+                insertionSort(lista, DIMENSAO);
+                break;
+            case 3:
+                selectionSort(lista, DIMENSAO);
+                break;
+            case 4:
+                quickSort(lista, 0, DIMENSAO-1);
+                break;
+            default:
+                printf("Comando nao encontrado.");
+                break;
+        }
+        printf("Depois da ordenacao:\n");
+        showVector(lista, DIMENSAO);
 
-    switch(comando){
-        case 1:
-            bubbleSort(lista, DIMENSAO);
-            break;
-        case 2:
-            insertionSort(lista, DIMENSAO);
-            break;
-        case 3:
-            selectionSort(lista, DIMENSAO);
-            break;
-        case 5:
-            quickSort(lista, DIMENSAO);
-            break;
-        default:
-            printf("Comando nao encontrado.");
-            break;
-    }
-    printf("Depois da ordenacao:\n");
-    showVector(lista, DIMENSAO);
+    }while(comando);
+
 
     return 0;
 }

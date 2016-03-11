27241ebf628fa1bbc068a555e1afb46b2fb3d0e9
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-03-11 19:33:34 -0300

    Agora vai dar certo

--- Lista_05/main.c
+++ Lista_05/main.c
@@ -3,44 +3,48 @@
 #include <string.h>
 #include <ctype.h>
 
-#define NUMERO 1
-#define OPERADOR 2
+/*Cada elemento pode ser um
+Numero ou um operador.
+Caso o caractere seja '_'
+    o valor significa
+    um Numero
+Senão
+    O valor significa um Operador
+    no char operador.
+*/
 
 typedef struct no{
-    int tipo;
     int numero;
     char operador;
     struct no *proximo;
 }No;
 
-void imprimir(No *no){
-
-    if(no->tipo == NUMERO){
-        printf("%d", no->numero);
-    }
-    else if(no->tipo == OPERADOR){
-        printf("%c", no->operador);
-
-    }
-
-}
 
 int main()
 {
     char palavra[100];
+    char car;
     char subPalavra[100];
     int i;
     printf("Digite a expressao: ");
-    gets(palavra);
+    scanf("%s", palavra);
     printf("%s", palavra);
 
     for(i = 0; palavra[i] != '\0'; i++){
-
+        subPalavra[i] = palavra[i];
         if(!isdigit(palavra[i])){
-            //Aqui é um operador.
+ 
+		car = palavra[i];
+		subPalavra[i] = '\0';
+		strcpy(&palavra[0], &palavra[i+1]);
+		i = 0;
+        }else
+            continue;
 
-        }
-        printf("\n%c\n", palavra[i]);
+        printf("\nPalavra: %s\n", palavra);
+        printf("\ncaracter %c", car);
+        printf("\n\nSubpalavra: %s", subPalavra);
+	
 
 
     }
